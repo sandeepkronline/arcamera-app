7207e122b4c35103e6c6d2e696be0f34bf520365
Sandeep KR <sandeep.kr@trenser.com>
2021-10-08 13:39:38 +0900

    Initial commit

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ function loadMarkers(){
 	var nftNode = document.getElementById("#marker");
 	nftNode.setAttribute("url", nftUrl);
 	
-	if( modelType == "3d1"){
+	if( modelType == "3d"){
 		load3dModles();
 	}
-	else if( modelType == "2d1"){
+	else if( modelType == "2d"){
 		loadImages();
 	}
-	else if( modelType == "text1")
+	else if( modelType == "text")
 	{
 		loadText()
 	}
@@ -40,22 +40,25 @@ function load3dModles(){
 	let modelId = urlParams.get("model")
 	
 	const markerDiv = document.createElement("a-gltf-model");
+	markerDiv.setAttribute("material","transparent:true;shader:flat;side:double;");
+	markerDiv.setAttribute("rotation", "0 0 0" );
+	markerDiv.setAttribute("position", "0 0 0" );
 	if( modelId == "1"){
 		markerDiv.setAttribute("gltf-model", "models/3D/melite.gltf");
 		markerDiv.setAttribute("scale", "50 50 50");
-		markerDiv.setAttribute("position", "100 -300 0" );	
+		//markerDiv.setAttribute("position", "100 -300 0" );	
 	}
 	else if (modelId == "2"){
 		markerDiv.setAttribute("gltf-model", "models/3D/skyscraper.gltf");
 		markerDiv.setAttribute("scale", "10 10 10");
-		markerDiv.setAttribute("position", "100 -300 0" );
-		markerDiv.setAttribute("rotation", "0 0 0" );	
+		//markerDiv.setAttribute("position", "100 -300 0" );
+		//markerDiv.setAttribute("rotation", "0 0 0" );	
 	}
 	else{
 		markerDiv.setAttribute("gltf-model", "models/3D/valentim.gltf");
 		markerDiv.setAttribute("scale", "10 10 10");
-		markerDiv.setAttribute("position", "100 -300 0" );
-		markerDiv.setAttribute("rotation", "0 45 45" );
+		//markerDiv.setAttribute("position", "100 -300 0" );
+		//markerDiv.setAttribute("rotation", "0 45 45" );
 		
 	}
 	var element = document.getElementById("#marker");
@@ -66,16 +69,18 @@ function loadImages(){
 	const urlParams = new URLSearchParams(window.location.search)
 	let modelId = urlParams.get("model")
 	const markerDiv = document.createElement("a-image");
+	markerDiv.setAttribute("rotation", "0 0 0" );
+	markerDiv.setAttribute("position", "0 0 0" );
 	if( modelId == "1"){
 		markerDiv.setAttribute("src", "models/Image/butterflies.gif");
 		markerDiv.setAttribute("scale", "100 100 100");
-		markerDiv.setAttribute("rotation", "-90 0 0");	
-		markerDiv.setAttribute("position", "100 -300 0" );
+		//markerDiv.setAttribute("rotation", "-90 0 0");	
+		//markerDiv.setAttribute("position", "100 -300 0" );
 	}else{
 		markerDiv.setAttribute("src", "models/Image/love.png");
 		markerDiv.setAttribute("scale", "100 100 100");
-		markerDiv.setAttribute("rotation", "-90 0 0");	
-		markerDiv.setAttribute("position", "100 -300 0" );
+		//markerDiv.setAttribute("rotation", "-90 0 0");	
+		//markerDiv.setAttribute("position", "100 -300 0" );
 	}
 	var element = document.getElementById("#marker");
 	element.appendChild(markerDiv);	
@@ -89,8 +94,10 @@ function loadText(){
 	markerDiv.setAttribute("value", modelId);
 	markerDiv.setAttribute("scale", "500 500 500");
 	markerDiv.setAttribute("color", "black");
-	markerDiv.setAttribute("rotation", "0 0 45");
-	markerDiv.setAttribute("position", "100 -100 50" );
+	//markerDiv.setAttribute("rotation", "0 0 45");
+	//markerDiv.setAttribute("position", "100 -100 50" );
+	markerDiv.setAttribute("rotation", "0 0 0" );
+	markerDiv.setAttribute("position", "0 0 0" );
 	var element = document.getElementById("#marker");
 	element.appendChild(markerDiv);	
 }
